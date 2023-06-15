1617349ecb1cf80d020e86a9c7742ca51a4114bb
hailey ham <roserealslow@gmail.com>
2023-06-15 17:45:54 +0900

    #4 Chore :redux_toolkit 기본셋팅 및 예시등록

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/store"
+
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
