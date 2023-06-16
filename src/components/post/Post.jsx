fd581856b7ee8773d2d4282893251ef0c9240112
ungHeung <t01047377477@gmail.com>
2023-06-17 04:59:12 +0900

    Feat : 피드 포스트 컴포넌트 마크업, 스타일 추가

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import iconHeart from '../../assets/icons/post/icon-heart.svg';
+import iconHeartFill from '../../assets/icons/post/icon-heart-fill.svg';
+import iconComment from '../../assets/icons/post/icon-comment.svg';
+
+export default function Post() {
+    return (
+        <Li>
+            <a href="#">
+                <ProfileWrap>
+                    <ProfileLeft>
+                        <ImgProfile
+                            src={}
+                            alt="유저 프로필"
+                        />
+                    </ProfileLeft>
+                    <ProfileRight>
+                        <UserName>{}</UserName>
+                        <UserId>@ {}</UserId>
+                    </ProfileRight>
+                </ProfileWrap>
+                <ImageWrap>
+                    {}
+                </ImageWrap>
+                <ContentWrap>
+                    <p>{}</p>
+                    <span>{}</span>
+                </ContentWrap>
+                <EtcWrap>
+                    <img
+                        src={iconHeart}
+                        alt="좋아요"
+                    />
+                    <span>{}</span>
+                    <img src={iconComment} alt="댓글" />
+                    <span>{}</span>
+                </EtcWrap>
+            </a>
+        </Li>
+    );
+}
+
+const Li = styled.li`
+    width: 350px;
+    margin: 0 auto;
+    border-radius: var(--radius-input);
+    color: var(--color-gray);
+`;
+
+const UserName = styled.strong`
+    display: block;
+    font-size: var(--fsize-title);
+`;
+
+const UserId = styled.span`
+    font-family: var(--font-eng);
+    font-size: var(--fsize-title);
+    font-style: italic;
+    color: var(--color-gray);
+`;
+
+const SectionDefault = styled.section`
+    width: 100%;
+    box-sizing: border-box;
+`;
+
+const ProfileWrap = styled(SectionDefault)`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 20px;
+`;
+
+const ProfileLeft = styled.section``;
+
+const ImgProfile = styled.img`
+    width: 30px;
+    vertical-align: middle;
+`;
+
+const ProfileRight = styled.section`
+    line-height: 12px;
+`;
+
+const ImageWrap = styled(SectionDefault)`
+    width: 100%;
+`;
+
+const ImgContent = styled.img`
+    width: 350px;
+    height: 192px;
+    object-fit: contain;
+`;
+
+const ContentWrap = styled(SectionDefault)`
+    display: -webkit-box;
+    max-height: 65px;
+    padding: 20px;
+    font-size: var(--fsize-cont-thumb);
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+`;
+
+const EtcWrap = styled(SectionDefault)`
+    padding: 20px;
+    text-align: right;
+
+    & > img {
+        width: 12px;
+        margin-right: 5px;
+        vertical-align: middle;
+    }
+
+    & > span {
+        font-family: var(--font-eng);
+        font-size: 14px;
+        font-style: italic;
+        color: var(--color-gray);
+    }
+
+    & > span:first-of-type {
+        margin-right: 5px;
+    }
+`;
