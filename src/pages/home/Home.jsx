c06d8dce5c25d9cc73c358940a1eac94b6872d96
ungHeung <t01047377477@gmail.com>
2023-06-17 04:46:37 +0900

    Feat : 홈 메인 피드 마크업, 스타일 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,23 @@ import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import Common from '../../components/design/main/Common';
 import { styled } from 'styled-components';
+import divLine from '../../assets/icons/post/div-line.svg';
 
 export default function Home() {
+    const page = (
+        <>
+            <CategoryNav>
+                <CategoryButton>전체 글</CategoryButton>
+                <DivLine src={divLine} alt="" />
+                <CategoryButton>내 글</CategoryButton>
+                <DivLine src={divLine} alt="" />
+                <CategoryButton>친구 글</CategoryButton>
+            </CategoryNav>
+
+            <ul></ul>
+        </>
+    );
+
     return (
         <>
             <Header />
@@ -13,3 +28,25 @@ export default function Home() {
         </>
     );
 }
+
+const CategoryNav = styled.article`
+    width: 100%;
+    text-align: right;
+`;
+
+const CategoryButton = styled.button`
+    all: unset;
+    font-size: var(--fsize-button-sub);
+    color: var(--color-gray);
+
+    &:focus,
+    &:hover {
+        outline: none;
+        color: var(--color-main);
+    }
+`;
+
+const DivLine = styled.img`
+    margin: 10px;
+    vertical-align: middle;
+`;
