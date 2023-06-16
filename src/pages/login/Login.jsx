4d5590f3dd5a1bf86df601b7b1c409c6f8f1dc60
ungHeung <t01047377477@gmail.com>
2023-06-17 04:33:58 +0900

    Feat : 입력값을 사용한 로그인 및 토큰 저장 기능 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -45,6 +45,41 @@ export default function Login() {
     );
 }
 
+const URL = 'https://api.mandarin.weniv.co.kr';
+
+async function handleLogin() {
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    const requestPath = '/user/login';
+    const requestUrl = `${URL}${requestPath}`;
+
+    const userData = {
+        user: {
+            email: email,
+            password: password,
+        },
+    };
+
+    const response = await fetch(requestUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    const json = await response.json();
+    const token = json.user.token;
+    localStorage.setItem('token', token);
+
+    if (!localStorage.token.length) {
+        return false;
+    }
+
+    return true;
+}
+
 function checkToken() {
     const token = localStorage.token;
 
