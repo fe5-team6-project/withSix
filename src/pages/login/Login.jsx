55753e2d914f23709512ee386999b5871e2ac48a
ungHeung <t01047377477@gmail.com>
2023-06-17 04:31:18 +0900

    Feat : 유저 상태관리용 디스패치, 페이지 이동을 위한 네비게이트 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 export default function Login() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     return (
         <Form>
             <Div>
