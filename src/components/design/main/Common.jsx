457776f838b70ed4af1e2bdfac3436c89e483617
ungHeung <t01047377477@gmail.com>
2023-06-16 21:08:08 +0900

    Feat : 공통 메인 컴포넌트 추가

---
+++ src/components/design/main/Common.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Header from "../../header/Header";
+import Footer from "../../footer/Footer";
+import { styled } from "styled-components";
+
+export default function Common() {
+    return (
+        <>
+            <Header />
+            <StyledMain>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam amet nesciunt ea, libero vero voluptate debitis reprehenderit nam rem illum. Eveniet soluta aliquid libero culpa
+                    quasi dolore neque error saepe. Aliquid fuga tempore deleniti minima saepe aspernatur, quos incidunt nulla fugit accusantium est labore hic quis. Laborum tempore consequatur
+                    similique animi quia maxime eveniet corporis, corrupti tempora velit non? Accusantium. Molestias, voluptatum ullam! Magnam maiores tempore eligendi obcaecati, voluptates totam,
+                    nobis, dolores unde rerum hic quidem est fugiat inventore cumque perspiciatis natus ea voluptas? Voluptas soluta veniam blanditiis itaque at? Nemo tempora inventore eius
+                    reprehenderit quidem eum corrupti nostrum deleniti nesciunt, dolorem unde, esse ducimus ullam optio nihil rem error quae neque! Placeat odit esse expedita animi rerum natus
+                    veritatis. Quae provident doloremque placeat dolor praesentium cum, sed consequatur ex magni atque illum totam non modi fugit eius vero corporis sit perferendis quos, eos iure
+                    quia. Autem harum facilis dolorum. Cum aperiam soluta quia, aspernatur in unde libero ad aut labore voluptate consequuntur blanditiis porro vero? At obcaecati delectus ipsam
+                    officiis, corporis ut voluptates fugit reprehenderit unde quaerat sequi accusantium! Similique ab exercitationem facilis optio, quaerat laudantium in molestias maiores nisi sunt ut
+                    aliquam non consectetur ipsum suscipit eum dignissimos animi impedit, dolorem voluptatum temporibus laboriosam eligendi! Expedita, hic corrupti? Magnam aperiam atque officiis id
+                    soluta earum, inventore maxime unde natus, illum velit? Non pariatur inventore porro. Velit dicta deleniti voluptas, magni dolor nesciunt sapiente, perspiciatis dolore placeat,
+                    soluta voluptate? In quo, beatae distinctio, minus dolor maiores et perspiciatis similique eius maxime voluptatem magnam pariatur fugit accusamus. Dignissimos, officiis? Voluptas
+                    velit porro aliquid, obcaecati vel sapiente assumenda dolorem illum explicabo! Qui, quo. Sit omnis illo asperiores libero rerum ad unde, dolorem, mollitia deleniti facere maiores
+                    aliquam neque repellendus nihil ipsa, atque itaque eius in minus veritatis. Eius neque sapiente accusamus? Non repellat perspiciatis ipsum. Earum, tempore iure in amet esse
+                    praesentium expedita possimus qui! Necessitatibus ex facilis delectus alias non consectetur vitae officia similique distinctio, esse expedita eligendi eos quasi? Officia ad quaerat
+                    modi maiores repudiandae expedita perferendis debitis eos qui at neque, sunt commodi ullam inventore deleniti dolore accusantium asperiores nesciunt perspiciatis facere corrupti
+                    laudantium aliquam. Blanditiis, tenetur provident. Ea magni laboriosam voluptatem assumenda, vitae aliquid minima enim temporibus adipisci rerum numquam maxime, reprehenderit
+                    atque. Excepturi natus vitae assumenda placeat doloribus tempore quibusdam voluptatibus beatae veniam dolore, dolor nulla. Nihil fugiat optio eveniet rem quisquam quaerat esse
+                    ipsum, quia ad in dolor nostrum necessitatibus, magnam quas eligendi. Possimus atque beatae temporibus error laudantium minus quae? Nostrum labore officia minus? Sapiente ex
+                    dolorem beatae quasi assumenda, debitis, quisquam doloremque, consectetur sed expedita nemo. Deleniti eos quibusdam omnis blanditiis? Consectetur totam adipisci ut consequatur
+                    incidunt inventore nobis eius illo architecto veritatis? Voluptatibus sed, eaque officia deserunt quas aspernatur sapiente voluptas ut quod aperiam ab laborum quibusdam
+                    exercitationem nihil harum repellendus, unde aliquid voluptate vitae. Autem, voluptas magni. Voluptatem explicabo eaque enim? Iusto quibusdam tempora numquam beatae nihil sit quas.
+                    Saepe totam numquam magni doloremque possimus, veniam qui velit vitae asperiores, quasi aliquam odio aperiam debitis? Inventore voluptatum tempore dicta ipsa excepturi. Ipsam,
+                    consectetur cumque doloremque suscipit dicta ipsa aperiam, quis doloribus iure, fuga voluptatibus ullam id repudiandae unde! Eligendi quasi repellat, qui provident eos distinctio
+                    adipisci ratione fugiat vel mollitia odio? A provident excepturi officiis rem tempore laudantium dicta! Consequuntur aliquid exercitationem eum accusamus in temporibus itaque
+                    minima similique. Animi ex eum quos consequuntur eaque numquam nemo non quisquam labore quaerat. Modi distinctio esse porro, sunt, quisquam rerum explicabo ad quod nihil sapiente
+                    amet deserunt? Nulla neque hic, sed id incidunt ratione quidem assumenda doloribus, ex labore est corporis eum consequuntur? Corporis praesentium ipsam cum asperiores ut iure
+                    incidunt sunt minima pariatur excepturi cumque accusantium quae placeat officia magnam, soluta animi debitis, laudantium eum enim ipsum? Vitae minima fugiat temporibus distinctio!
+                    Optio non voluptates dignissimos quibusdam numquam suscipit minus facere cupiditate doloribus quidem veritatis, nihil unde sit velit aliquid dolores debitis corporis minima dolor?
+                    Sint vero ipsum atque corrupti eos aperiam? Mollitia rerum minima dicta libero perferendis doloremque at dolorem quia eos veniam molestias voluptas officiis corrupti dolor in
+                    facere id quisquam vel accusantium, explicabo sequi. Commodi modi laboriosam quod nesciunt! Nesciunt magnam odit voluptatem deleniti harum aliquid suscipit earum. Id non eos
+                    accusantium voluptate. Placeat quisquam saepe possimus ipsa explicabo, aperiam quos delectus quas voluptatum recusandae sapiente, sequi dignissimos numquam? Architecto nihil
+                    expedita excepturi a nam, blanditiis asperiores officia in, ea aspernatur voluptatum vitae sapiente et odit accusantium nemo maxime harum illo. Animi, perspiciatis pariatur amet
+                    provident ratione voluptatem repellat! Deserunt voluptates suscipit tempora recusandae, aperiam fuga! Reiciendis, quis ullam voluptatum ducimus sunt expedita placeat fugit iste
+                    molestias reprehenderit facere delectus deleniti odio impedit ipsam, dolore mollitia perspiciatis est aspernatur! Ducimus perspiciatis eius obcaecati corporis earum eveniet
+                    voluptatibus dolore? Fugiat quos doloremque et fugit maiores modi porro neque. Nam, perferendis? Iste, atque hic excepturi reprehenderit autem saepe quod officiis non. Quasi
+                    dolorum atque alias, blanditiis ut eos explicabo eum unde reiciendis consequuntur, dolorem sit saepe repellat! Necessitatibus ratione perferendis reiciendis pariatur dolores,
+                    consequuntur possimus inventore sunt harum. Vel, perferendis dignissimos. Nesciunt velit molestias eos dolore fuga esse repellendus, quisquam itaque, molestiae deserunt modi quidem
+                    officia debitis labore sed totam, sequi omnis. Illum libero officia aut rerum voluptatibus ratione quisquam animi. Necessitatibus molestias sed adipisci excepturi illo omnis non
+                    recusandae, veritatis nostrum eos fugit in dolorum quod inventore! Vel nihil sed in nisi, molestiae mollitia fugit nostrum quas reiciendis tempore esse.
+                </div>
+            </StyledMain>
+            <Footer />
+        </>
+    );
+}
+
+const StyledMain = styled.main`
+    width: 100%;
+    max-width: 900px;
+    max-height: calc(100vh - 120px);
+    margin: 70px auto 50px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    overflow-y: auto;
+`;
