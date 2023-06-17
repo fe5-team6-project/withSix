7cf71aea808ca2ee075789dc6e2edda8c3408765
ungHeung <t01047377477@gmail.com>
2023-06-18 02:36:53 +0900

    Feat : 받아온 정보로 회원가입 요청 기능

--- src/pages/signup/Profile.jsx
+++ src/pages/signup/Profile.jsx
@@ -34,6 +34,44 @@ export default function Profile(props) {
 
 const URL = 'https://api.mandarin.weniv.co.kr';
 
+async function handleSignup(user) {
+    const img = document.querySelector('#image').file;
+    const id = document.querySelector('#id').value;
+    const nickname = document.querySelector('#name').value;
+    const intro = document.querySelector('#intro').value;
+
+    const email = user.email;
+    const password = user.password;
+
+    const requestPath = '/user';
+    const requestUrl = `${URL}${requestPath}`;
+
+    const userData = {
+        user: {
+            username: nickname,
+            email: email,
+            password: password,
+            accountname: id,
+            intro: intro,
+            image: img,
+        },
+    };
+
+    const response = await fetch(requestUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    const json = response.json();
+    const message = response.message;
+
+    alert(message);
+    return json;
+}
+
 async function validation() {
     const input = document.querySelector('#id');
     const id = input.value;
