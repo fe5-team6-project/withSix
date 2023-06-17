69a80d86be01a8e2ea03b09a887106908f5e6bf0
ungHeung <t01047377477@gmail.com>
2023-06-17 23:06:14 +0900

    Chore : 회원가입 라우트 경로 추가

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,9 +1,12 @@
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/main/Main";
-import Home from "../pages/home/Home";
-import TogetherUpload from "../pages/together/TogetherUpload";
-import TogetherEdit from "../pages/together/TogetherEdit";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../pages/main/Main';
+import Home from '../pages/home/Home';
+import TogetherUpload from '../pages/together/TogetherUpload';
+import TogetherEdit from '../pages/together/TogetherEdit';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from '../pages/main/Main';
+import Home from '../pages/home/Home';
+import Signup from '../pages/signup/Signup';
 
 export default function MyRouter() {
     return (
@@ -12,10 +15,14 @@ export default function MyRouter() {
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/home" element={<Home />} />
-                    <Route path="/togetherUpload" element={<TogetherUpload />} />
+                    <Route
+                        path="/togetherUpload"
+                        element={<TogetherUpload />}
+                    />
                     <Route path="/togetherEdit" element={<TogetherEdit />} />
+                    <Route path="/signup" element={<Signup />} />
                 </Routes>
             </BrowserRouter>
         </>
-    )
+    );
 }
