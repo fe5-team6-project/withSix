d01dd0870f88ba4d64fcd6ee841196c804e7187f
ungHeung <t01047377477@gmail.com>
2023-06-17 13:37:01 +0900

    Chore : 라우트 설정 추가

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,12 +1,15 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/main/Main";
+import Home from "../pages/home/Home";
 
 export default function MyRouter() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<></>} />
+          <Route path="/" element={<Main />} />
+          <Route path="/home" element={<Home /> } />
         </Routes>
       </BrowserRouter>
     </>
