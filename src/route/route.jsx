12982ba46e97ed3674a8d63f11930c2dd18e9763
hailey ham <roserealslow@gmail.com>
2023-06-18 00:57:02 +0900

    chore : 모임수정페이지 라우트 연결

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/main/Main";
 import Home from "../pages/home/Home";
 import TogetherUpload from "../pages/together/TogetherUpload";
+import TogetherEdit from "../pages/together/TogetherEdit";
 
 export default function MyRouter() {
     return (
@@ -12,6 +13,7 @@ export default function MyRouter() {
                     <Route path="/" element={<Main />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/togetherUpload" element={<TogetherUpload />} />
+                    <Route path="/togetherEdit" element={<TogetherEdit />} />
                 </Routes>
             </BrowserRouter>
         </>
