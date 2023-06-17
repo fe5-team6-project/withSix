831eb58b26ab142ed18c8efe93e8be7c9835baf7
ungHeung <t01047377477@gmail.com>
2023-06-18 02:25:59 +0900

    Feat : 회원가입 단계 상태, 이메일, 비밀번호 상태 관리

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,31 +1,11 @@
-import React from 'react';
-import IdPassword from './IdPassword';
-import Profile from './Profile';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Signup() {
-    const navigator = useNavigate();
+    const [passStep, setPassStep] = useState(false);
+    const [user, setUser] = useState({
+        username: String,
+        email: String,
+    });
 
-    const movePage = () => {
-        if (!checkToken) {
-            return false;
-        } else {
-            navigator('/');
-        }
-    };
-
-    return (
-        <>
-            <IdPassword />
-            <Profile />
-        </>
-    );
-}
-
-function checkToken() {
-    const token = localStorage.token;
-    if (!token) {
-        return false;
-    }
-    return true;
+    return <></>;
 }
