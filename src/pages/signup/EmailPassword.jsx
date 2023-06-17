ad58c5f2af3d8b83232101d8ae20e551c2ba188f
ungHeung <t01047377477@gmail.com>
2023-06-18 02:32:53 +0900

    Feat : 이메일 중복 검사, 이메일, 비밀번호 상태값 전송하는 기능 추가

--- src/pages/signup/EmailPassword.jsx
+++ src/pages/signup/EmailPassword.jsx
@@ -4,7 +4,24 @@ import { styled } from 'styled-components';
 export default function EmailPassword(props) {
     return (
         <>
-            <Form>
+            <Form
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    const pass = validation();
+                    if (pass) {
+                        const email = document.querySelector('#email').value;
+                        const password =
+                            document.querySelector('#password').value;
+                        props.userData({
+                            email: email,
+                            password: password,
+                        });
+                        props.passStep(pass);
+                    } else {
+                        return false;
+                    }
+                }}
+            >
                 <Div>
                     <Input type="text" id="email" placeholder=" " />
                     <Label htmlFor="email">E-mail</Label>
@@ -25,6 +42,48 @@ export default function EmailPassword(props) {
             </Form>
         </>
     );
+}
+
+const URL = 'https://api.mandarin.weniv.co.kr';
+
+async function validation() {
+    const input = document.querySelector('#email');
+    const email = input.value;
+
+    const requestPath = '/user/emailvalid';
+    const requestUrl = `${URL}${requestPath}`;
+
+    const userData = {
+        user: {
+            email: email,
+        },
+    };
+
+    const response = await fetch(requestUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    const json = await response.json();
+    const message = json.message;
+
+    if (!response.of) {
+        alert(message);
+        input.focus();
+        return false;
+    } else {
+        if (message.match('')) {
+            alert(message);
+            input.focus();
+            return false;
+        }
+    }
+
+    alert(message);
+    return true;
 }
 
 const Form = styled.form`
