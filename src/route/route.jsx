afef0e0a58252c80715d5084daa23225cec60d76
ungHeung <t01047377477@gmail.com>
2023-06-18 01:28:17 +0900

    Fix : 라우트 병합, 충돌 수정

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -3,9 +3,6 @@ import Main from '../pages/main/Main';
 import Home from '../pages/home/Home';
 import TogetherUpload from '../pages/together/TogetherUpload';
 import TogetherEdit from '../pages/together/TogetherEdit';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from '../pages/main/Main';
-import Home from '../pages/home/Home';
 import Signup from '../pages/signup/Signup';
 
 export default function MyRouter() {
