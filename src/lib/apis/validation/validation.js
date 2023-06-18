534dd0c87066a990960b1a0b4c71d1d9056890c1
ungHeung <t01047377477@gmail.com>
2023-06-18 21:05:55 +0900

    Feat : 로그인 이메일, 비밀번호 확인 및 오류메시지 출력 기능

--- src/lib/apis/validation/validation.js
+++ src/lib/apis/validation/validation.js
@@ -1,3 +1,17 @@
-export const validation = {
-    regEmail: () => {},
+import { FAIL_NULL_EMAIL, FAIL_NULL_PASSWORD } from '../constant/message';
+
+export const validationLogin = (email, password) => {
+    const lenEmail = email.length;
+    const lenPassword = password.length;
+
+    if (!lenEmail) {
+        alert(FAIL_NULL_EMAIL);
+        return false;
+    }
+    if (!lenPassword) {
+        alert(FAIL_NULL_PASSWORD);
+        return false;
+    }
+
+    return true;
 };
