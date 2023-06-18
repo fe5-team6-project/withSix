aa637effd7a5d1361690038707751b20070ebc58
ungHeung <t01047377477@gmail.com>
2023-06-18 21:10:10 +0900

    Feat : 회원가입 비밀번호 유효성 검사, 오류 메시지 출력 기능

--- src/lib/apis/validation/validation.js
+++ src/lib/apis/validation/validation.js
@@ -1,10 +1,12 @@
 import {
     FAIL_ALEADY_EMAIL,
     FAIL_FORM_EMAIL,
+    FAIL_FORM_PASSWORD,
+    FAIL_LENGTH_PASSWORD,
     FAIL_NULL_EMAIL,
     FAIL_NULL_PASSWORD,
 } from '../constant/message';
-import { REG_EXP_EMAIL } from '../constant/regexp';
+import { REG_EXP_EMAIL, REG_EXP_PASSWORD } from '../constant/regexp';
 
 export const validationLogin = (email, password) => {
     const lenEmail = email.length;
@@ -37,6 +39,28 @@ export const validationEmail = async (email) => {
 
     if (!(await checkDuplicationEmail())) {
         alert(FAIL_ALEADY_EMAIL);
+        return false;
+    }
+
+    return true;
+};
+
+export const validationPassword = (password) => {
+    const length = password.length;
+    const [minLen, maxLen] = [6, 16];
+
+    if (!length) {
+        alert(FAIL_NULL_PASSWORD);
+        return false;
+    }
+
+    if (maxLen < length || length < minLen) {
+        alert(FAIL_LENGTH_PASSWORD);
+        return false;
+    }
+
+    if (!REG_EXP_PASSWORD.test(password)) {
+        alert(FAIL_FORM_PASSWORD);
         return false;
     }
 
