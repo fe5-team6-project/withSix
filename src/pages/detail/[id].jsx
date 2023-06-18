5ced32f010abeb45a766c78e4f472d55fdb67058
hyuntae <believe4279@gmail.com>
2023-06-18 20:53:25 +0900

    #22 Feat: 게시글상세목록

--- src/pages/detail/[id].jsx
+++ src/pages/detail/[id].jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { api } from '../../lib/apis/axiosConfig';
 import { returnErrorMessage } from './utils/errorMessage';
+import { URL } from '../../lib/apis/constants';
 
-const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZkN2I3YjJjYjIwNTY2MzJkMDA5MyIsImV4cCI6MTY5MDgxNzQyOCwiaWF0IjoxNjg1NjMzNDI4fQ.fuRi1qVjgU4C7my-RPJrPOoBFjAvSHauogh8alP9mbI';
 const postId = '6478c001b2cb2056632d23f2';
 
 export default function PostDetail() {
     const { id } = useParams();
     const [data, setData] = useState('');
+    const [commentCount, setCommentCount] = useState('');
 
     const fetchData = async () => {
         try {
@@ -17,6 +17,7 @@ export default function PostDetail() {
                 data: { post },
             } = await api.get(`/post/${id}`);
             setData(post);
+            setCommentCount(post.commentCount);
         } catch (error) {
             returnErrorMessage(error);
         }
@@ -28,5 +29,25 @@ export default function PostDetail() {
 
     if (!data) return <h1>Loading</h1>;
 
-    return <div>Detail Page</div>;
+    return (
+        <>
+            <p>{data.content}</p>
+            <img src={data.author.image}></img>
+            {data.image.split(', ').map((img) => (
+                <img
+                    key={img}
+                    src={URL + '/' + img}
+                    width={100}
+                    height={100}
+                    alt="이미지"
+                ></img>
+            ))}
+            <p>{data.author.username} </p>
+            <p>@{data.author.accountname}</p>
+            <h3>댓글 개수 : {commentCount}</h3>
+            {/* todo
+                생성날짜변환 ex)2020년_10월_21일 */}
+            <p>{data.createdAt}</p>
+        </>
+    );
 }
