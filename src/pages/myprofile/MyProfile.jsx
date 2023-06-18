a28c65f2ec1f70945371719d11a55d098a45153e
ungHeung <t01047377477@gmail.com>
2023-06-18 10:15:35 +0900

    Design : 마이프로필 스타일 적용

--- src/pages/myprofile/MyProfile.jsx
+++ src/pages/myprofile/MyProfile.jsx
@@ -2,48 +2,55 @@ import React from 'react';
 import Common from '../../components/design/main/Common';
 import { Link } from 'react-router-dom';
 import logout from '../../assets/icons/common/icon-logout.svg';
+import { styled } from 'styled-components';
 
 export default function MyProfile() {
     const page = (
-        <>
-            <section>
+        <LayoutDiv>
+            <Section>
                 <h2 className="a11y-hidden">프로필 이미지</h2>
                 <img src="" alt="" />
-            </section>
+            </Section>
 
-            <section>
+            <Section>
                 <h2 className="a11y-hidden">팔로우</h2>
-                <h3>Followers</h3>
-                <Link to={''}>{9999}</Link>
-                <h3>Followings</h3>
-                <Link to={''}>{9999}</Link>
-            </section>
+                <FollowDiv className="followers">
+                    <Label>Followers</Label>
+                    <FollowLink to={''}>{9999}</FollowLink>
+                </FollowDiv>
+                <FollowDiv className="followings">
+                    <Label>Followings</Label>
+                    <FollowLink to={''}>{9999}</FollowLink>
+                </FollowDiv>
+            </Section>
 
-            <section>
+            <Section>
                 <h2 className="a11y-hidden">프로필</h2>
-                <div>
-                    <span>ID</span>
-                    <strong></strong>
-                </div>
-                <div>
-                    <span>Nickname</span>
-                    <strong></strong>
-                </div>
-                <div>
-                    <span>Introduce</span>
-                    <strong></strong>
-                </div>
-            </section>
+                <Div className="id">
+                    <Label>ID</Label>
+                    <Strong>{'ungheung'}</Strong>
+                </Div>
+                <Div>
+                    <Label>Nickname</Label>
+                    <Strong>{'ungheung'}</Strong>
+                </Div>
+                <Div>
+                    <Label>Introduce</Label>
+                    <Strong>{'자기소개입니다.'}</Strong>
+                </Div>
+            </Section>
 
-            <section>
+            <Section>
                 <span></span>
-            </section>
-            <button>프로필 수정</button>
-            <Link to={'/'}>
-                <img src={logout} alt="로그아웃 아이콘" />
-                Logout
-            </Link>
-        </>
+            </Section>
+            <Div>
+                <button>프로필 수정</button>
+                <LogoutLink to={'/'}>
+                    <img src={logout} alt="로그아웃 아이콘" />
+                    Logout
+                </LogoutLink>
+            </Div>
+        </LayoutDiv>
     );
 
     const pageTitle = '프로필';
@@ -55,3 +62,80 @@ export default function MyProfile() {
         </>
     );
 }
+
+const LayoutDiv = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+    width: 390px;
+    height: calc(100vh - 120px);
+    margin: 0 auto;
+    padding: 40px 0;
+    box-sizing: border-box;
+`;
+
+const Section = styled.section`
+    width: 240px;
+`;
+
+const Div = styled.div`
+    position: relative;
+    height: 70px;
+    text-align: center;
+`;
+
+const FollowDiv = styled(Div)`
+    width: 50%;
+
+    &.followers {
+        float: left;
+    }
+
+    &.followings {
+        float: right;
+    }
+`;
+
+const FollowLink = styled(Link)`
+    font-size: var(--fsize-title);
+    font-style: italic;
+    font-weight: bold;
+    color: var(--color-main);
+`;
+
+const Label = styled.label`
+    display: block;
+    width: 240px;
+    font-size: var(--fsize-s);
+    font-style: italic;
+    font-weight: lighter;
+    color: var(--color-gray);
+    text-align: left;
+`;
+
+const Strong = styled.strong`
+    color: var(--color-main);
+
+    ${Div}.id > & {
+        font-family: var(--font-eng);
+        font-style: italic;
+    }
+    ${Div}.id > &::before {
+        content: '@';
+    }
+`;
+
+const LogoutLink = styled(Link)`
+    position: absolute;
+    right: 5px;
+    bottom: 0;
+    font-size: var(--fsize-s);
+    color: var(--color-gray);
+
+    & > img {
+        margin-right: 5px;
+    }
+`;
