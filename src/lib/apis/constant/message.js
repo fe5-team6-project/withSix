319b5a724152d909705c75187e0d48f32c149843
ungHeung <t01047377477@gmail.com>
2023-06-18 22:01:33 +0900

    Feat : 모달 출력 메시지 관리

--- src/lib/apis/constant/message.js
+++ src/lib/apis/constant/message.js
@@ -7,10 +7,14 @@ export const PROFILE_UPDATE_OK = '프로필 수정 성공!';
 export const FAIL_ACCESS = '잘못된 접근입니다.';
 export const FAIL_ALEADY_EMAIL = '이미 등록된 이메일입니다.';
 export const FAIL_ALEADY_ID = '이미 사용중인 아이디입니다.';
+export const FAIL_FORM_EMAIL = '올바른 이메일 형식이 아닙니다.';
+export const FAIL_FORM_PASSWORD = '하나 이상의 문자, 숫자가 포함되어야 합니다.';
+export const FAIL_FORM_ID = '영문, 숫자, (_), (,)만 사용할 수 있습니다.';
 export const FAIL_NULL_EMAIL = '이메일을 입력해주세요!';
 export const FAIL_NULL_PASSWORD = '비밀번호를 입력해주세요!';
 export const FAIL_NULL_ID = '아이디를 입력해주세요!';
 export const FAIL_NULL_NAME = '닉네임을 입력해주세요!';
-
-export const FAIL_VALID_ID_PASSWORD = '이메일 또는 비밀번호가 다릅니다.';
+export const FAIL_LENGTH_PASSWORD = '비밀번호는 6자 이상 16자 이하여야 합니다.';
 export const FAIL_CHECK_PASSWORD = '비밀번호를 확인해주세요.';
+
+export const FAIL_VALID_EMAIL_PASSWORD = '이메일 또는 비밀번호가 다릅니다.';
