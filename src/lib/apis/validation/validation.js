e861c2432e84280ed9676ec20939203956de9a5a
ungHeung <t01047377477@gmail.com>
2023-06-18 21:09:26 +0900

    Feat : 회원가입 이메일 유효성 검사, 오류 메시지 출력 기능

--- src/lib/apis/validation/validation.js
+++ src/lib/apis/validation/validation.js
@@ -1,4 +1,10 @@
-import { FAIL_NULL_EMAIL, FAIL_NULL_PASSWORD } from '../constant/message';
+import {
+    FAIL_ALEADY_EMAIL,
+    FAIL_FORM_EMAIL,
+    FAIL_NULL_EMAIL,
+    FAIL_NULL_PASSWORD,
+} from '../constant/message';
+import { REG_EXP_EMAIL } from '../constant/regexp';
 
 export const validationLogin = (email, password) => {
     const lenEmail = email.length;
@@ -10,6 +16,27 @@ export const validationLogin = (email, password) => {
     }
     if (!lenPassword) {
         alert(FAIL_NULL_PASSWORD);
+        return false;
+    }
+
+    return true;
+};
+
+export const validationEmail = async (email) => {
+    const length = email.length;
+
+    if (!length) {
+        alert(FAIL_NULL_EMAIL);
+        return false;
+    }
+
+    if (!REG_EXP_EMAIL.test(email)) {
+        alert(FAIL_FORM_EMAIL);
+        return false;
+    }
+
+    if (!(await checkDuplicationEmail())) {
+        alert(FAIL_ALEADY_EMAIL);
         return false;
     }
 
