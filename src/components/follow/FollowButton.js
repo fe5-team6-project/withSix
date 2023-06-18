3a71139932bc645fa1b94a8ab0488c81695f98e0
hailey ham <roserealslow@gmail.com>
2023-06-18 14:59:54 +0900

    Feat : 받아온 user isfollow정보로 버튼 내용 반영

--- src/components/follow/FollowButton.js
+++ src/components/follow/FollowButton.js
@@ -3,11 +3,16 @@ import { styled } from 'styled-components'
 // import HandleFollow from './HandleFollow'
 import { api } from '../../lib/apis/axiosConfig'
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 
 export default function FollowButton() {
     //받아오는 유저정보의 accountname 넣어서 follow
     const userInfo = useSelector((state) => { return state.user.myInfo })
     // const dispatch = useDispatch();
+
+    //user정보 받아온 걸로 넣어놓기 / 지금은 아직 연결이 안되어서 임시로 useState사용
+    const [isFollow, setIsFollow] = useState(Boolean);
+
     const follow = async () => {
         try {
             //임시로 ${userInfo.accountname}대신에 test999
@@ -15,7 +20,10 @@ export default function FollowButton() {
             // const res = await api.post(`/profile/${userInfo.accountname}/follow`, []);
             //리덕스 상태 업로드 setUserInfo 변경함수 이용하여 응답내용 집어넣기
             // dispatch(setUserInfo(res.data.profile));
-            console.log(res)
+            console.log(res);
+            //useState에 isfollow 현재 정보 업데이트 > 팔로우 한 것이면 버튼에 언팔로우 출력
+            setIsFollow(res.data.profile.isfollow);
+            console.log(isFollow)
         } catch (error) {
             console.log(error.res);
         }
@@ -24,7 +32,10 @@ export default function FollowButton() {
 
     return (
         // <StyledButton onClick={HandleFollow}>Follow</StyledButton>
-        <FollowUnButton id="FollowBtn" onClick={follow}>팔로우</FollowUnButton>
+        // <FollowUnButton id="FollowBtn" onClick={follow}>팔로우</FollowUnButton>
+        <FollowUnButton id="FollowBtn" onClick={follow}>
+            {isFollow ? '언팔로우' : '팔로우'}
+        </FollowUnButton>
     )
 }
 
