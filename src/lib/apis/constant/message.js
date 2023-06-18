c9a56e6ccfc705a2d4125b18712393d68a7821f7
ungHeung <t01047377477@gmail.com>
2023-06-18 19:40:50 +0900

    Refactor : 상수 분리

---
+++ src/lib/apis/constant/message.js
@@ -0,0 +1,16 @@
+/** modal message */
+export const LOGIN_OK = '로그인 성공!';
+export const LOGOUT_OK = '로그아웃 성공!';
+export const SIGNUP_OK = '가입 성공!';
+export const PROFILE_UPDATE_OK = '프로필 수정 성공!';
+
+export const FAIL_ACCESS = '잘못된 접근입니다.';
+export const FAIL_ALEADY_EMAIL = '이미 등록된 이메일입니다.';
+export const FAIL_ALEADY_ID = '이미 사용중인 아이디입니다.';
+export const FAIL_NULL_EMAIL = '이메일을 입력해주세요!';
+export const FAIL_NULL_PASSWORD = '비밀번호를 입력해주세요!';
+export const FAIL_NULL_ID = '아이디를 입력해주세요!';
+export const FAIL_NULL_NAME = '닉네임을 입력해주세요!';
+
+export const FAIL_VALID_ID_PASSWORD = '이메일 또는 비밀번호가 다릅니다.';
+export const FAIL_CHECK_PASSWORD = '비밀번호를 확인해주세요.';
---
+++ src/lib/apis/constant/regexp.js
@@ -0,0 +1,4 @@
+/** regexp */
+export const REG_EXP_EMAIL = /^[A-Za-z0-9_.-]+@[A-Za-z0-9-]+\.[A-Za-z0-9-]+/;
+export const REG_EXP_ID = /^[a-zA-Z0-9_,]/;
+export const REG_EXP_PASSWORD = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/;
