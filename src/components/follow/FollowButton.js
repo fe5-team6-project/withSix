3d171f16f2d9b1fdf33b5689ee8ad6b3d694627b
hailey ham <roserealslow@gmail.com>
2023-06-19 10:14:00 +0900

    #24 Feat : 버튼 누를 때마다 팔로우/언팔로우 상태보내고 받아와서 버튼변경

--- src/components/follow/FollowButton.js
+++ src/components/follow/FollowButton.js
@@ -13,30 +13,11 @@ export default function FollowButton() {
     //user정보 받아온 걸로 넣어놓기 / 지금은 아직 연결이 안되어서 임시로 useState사용
     const [isFollow, setIsFollow] = useState(true);
 
-    const follow = async () => {
-        try {
-            //임시로 ${userInfo.accountname}대신에 test999
-            const res = await api.post(`/profile/test999/follow`, []);
-            // const res = await api.post(`/profile/${userInfo.accountname}/follow`, []);
-            //리덕스 상태 업로드 setUserInfo 변경함수 이용하여 응답내용 집어넣기
-            // dispatch(setUserInfo(res.data.profile));
-            console.log(res);
-            //useState에 isfollow 현재 정보 업데이트 > 팔로우 한 것이면 버튼에 언팔로우 출력
-            setIsFollow(res.data.profile.isfollow);
-            console.log(isFollow)
-        } catch (error) {
-            console.log(error.res);
-        }
-    };
-    // console.log(userInfo)
-
-
-
     //팔로우 상태이면 : 버튼에 '언팔로우'뜨기 +(버튼누르기)> 언팔로우 처리 + '팔로우'로 버튼 변경
     const handleFollow = async () => {
 
         //isFollow정보 받아와서 처리해야하는데 지금 userInfo 관리 안되고있어서 임시로 true넣어놓기 = isfollow값 받아왔을때 true
-        if (true) {
+        if (isFollow) {
             await api.delete(`/profile/test999/unfollow`, [])
                 .then((res) => {
                     console.log(res);
@@ -45,9 +26,15 @@ export default function FollowButton() {
                 .then(() => {
                     console.log(isFollow);
                 });
-            // setIsFollow(res.data.profile.isfollow);
-            // console.log(res.data.profile.isfollow)
-            // console.log(isFollow);
+        } else {
+            await api.post(`profile/test999/follow`, [])
+                .then((res) => {
+                    console.log(res);
+                    setIsFollow(res.data.profile.isfollow);
+                })
+                .then(() => {
+                    console.log(isFollow);
+                })
         }
     }
 
