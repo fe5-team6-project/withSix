dfe942cdb79dbc27558ca154ba4dbc3c6ba1273c
hyuntae <believe4279@gmail.com>
2023-06-19 10:23:08 +0900

    #22 Fix: 경로삭제

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -7,7 +7,6 @@ import Signup from '../pages/signup/Signup';
 import MyProfile from '../pages/myprofile/MyProfile';
 import FollowingList from '../pages/follow/FollowingList';
 import FollowerList from '../pages/follow/FollowerList';
-import PostDetail from '../pages/detail/[id]';
 
 export default function MyRouter() {
     return (
@@ -25,7 +24,6 @@ export default function MyRouter() {
                     <Route path="/following" element={<FollowingList />} />
                     <Route path="/follower" element={<FollowerList />} />
                     <Route path="/myprofile" element={<MyProfile />} />
-                    <Route path="/post/detail/:id" element={<PostDetail />} />
                 </Routes>
             </BrowserRouter>
         </>
