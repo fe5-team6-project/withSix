bfbd27eafaeb30199ce4596137a73e9282d1aad0
ungHeung <t01047377477@gmail.com>
2023-06-19 11:10:43 +0900

    #26 Fix : URL 임포트

--- src/components/post/validationImage.js
+++ src/components/post/validationImage.js
@@ -1,33 +1,38 @@
-import { DEFAULT_IMAGE } from '../../lib/apis/constant/path';
+import { DEFAULT_IMAGE, URL } from '../../lib/apis/constant/path';
 
 export function checkProfileImage(image) {
     let src = image;
-    console.log(image);
+    console.log(image, '<<====');
 
     if (!src) {
         src = DEFAULT_IMAGE;
+        console.log(src, '<==== 비어있음');
+        return src;
+    }
+
+    if (!src.includes('api') && !src.includes('Ellipse')) {
+        src = `${URL}/${src}`;
+        console.log(src, '<==== api, ellipse 없음');
         return src;
     }
 
     if (src.includes('api') && src.includes('Ellipse')) {
         src = DEFAULT_IMAGE;
+        console.log(src, '<==== api, Ellipse 둘 다 있음');
         return src;
     }
 
     if (src.split('://').length > 2) {
         console.log(src.split('://'));
         src = DEFAULT_IMAGE;
+        console.log(src, '<==== 주소가 두개 이상 합쳐짐');
         return src;
     }
 
-    if (!src.includes('api') && !src.includes('Ellipse')) {
-        src = `${URL}/${src}`;
-        return src;
-    }
-
+    console.log(image, '<=== 문제없음');
     return src;
 }
 
-export function emptyImage(e) {
+export function emptyProfileImage(e) {
     e.currentTarget.style.src = DEFAULT_IMAGE;
 }
