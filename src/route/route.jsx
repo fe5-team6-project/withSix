047a0d3771c113f873f000ee44d300e70c567984
hyuntae <believe4279@gmail.com>
2023-06-19 13:50:39 +0900

    #22 Feat: 라우터 추가

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -7,6 +7,7 @@ import Signup from '../pages/signup/Signup';
 import MyProfile from '../pages/myprofile/MyProfile';
 import FollowingList from '../pages/follow/FollowingList';
 import FollowerList from '../pages/follow/FollowerList';
+import PostDetail from '../components/postDetail';
 
 export default function MyRouter() {
     return (
@@ -24,6 +25,7 @@ export default function MyRouter() {
                     <Route path="/following" element={<FollowingList />} />
                     <Route path="/follower" element={<FollowerList />} />
                     <Route path="/myprofile" element={<MyProfile />} />
+                    <Route path="/post/detail/:id" element={<PostDetail />} />
                 </Routes>
             </BrowserRouter>
         </>
