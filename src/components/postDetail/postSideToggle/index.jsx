8b4bf1802648251eb81cbe2696f479274b36cfc8
hyuntae <believe4279@gmail.com>
2023-06-19 10:45:59 +0900

    #22 Feat: 게시글신고

--- src/components/postDetail/postSideToggle/index.jsx
+++ src/components/postDetail/postSideToggle/index.jsx
@@ -2,7 +2,8 @@ import { useSelector } from 'react-redux';
 import ModifyAndDelete from './ModfifyAndDelete';
 import Report from './Report';
 
-const userId = '6476d7b7b2cb2056632d0093'; //나중에 전역변수로 설정
+const userId = '6476d7b7b2cb2056632d009'; //나중에 전역변수로 설정
+// const userId = '6476d7b7b2cb2056632d0093'; //나중에 전역변수로 설정
 
 export default function PostSideToggle({ postAuthorId }) {
     // const userId = useSelector((state) => state.user);
