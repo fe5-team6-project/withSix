79dbc4b126837b71ac7c6e506a423384ac1c0052
hailey ham <roserealslow@gmail.com>
2023-06-20 12:44:30 +0900

    모임리스트페이지 마크업

--- src/pages/together/Together.jsx
+++ src/pages/together/Together.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { api } from '../../lib/apis/axiosConfig'
+import { styled } from 'styled-components';
+import Common from '../../components/main/Common';
+
 
 export default function Together() {
+    const togetherList = async () => {
+        const res = api.get(`/product/hailey_ha`)
+        console.log(res);
+    }
+
+    const page = (
+        <>
+            <TogetherSection>
+                <TogetherList>
+                    <div onClick={togetherList}>together</div>
+                </TogetherList>
+            </TogetherSection>
+        </>
+    )
     return (
-        <div>together</div>
+        <>
+            <Common page={page} />
+        </>
     )
 }
+
+const TogetherSection = styled.section`
+    margin:54px 0px 10px 0px;
+`
+
+const TogetherList = styled.ul`
+    display:flex;
+    gap:10px;
+`
