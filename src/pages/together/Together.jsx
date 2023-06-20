e4d28215ee7d0f186b70d2ac1635a6b80759c6e3
hailey ham <roserealslow@gmail.com>
2023-06-20 13:42:55 +0900

    #39 Feat: 모임리스트 리덕스 유저정보 받아오기

--- src/pages/together/Together.jsx
+++ src/pages/together/Together.jsx
@@ -3,11 +3,14 @@ import { api } from '../../lib/apis/axiosConfig'
 import { styled } from 'styled-components';
 import Common from '../../components/main/Common';
 import TogetherList from '../../components/together/TogetherList';
+import { useSelector } from 'react-redux';
 
 
 export default function Together() {
+    const myInfo = useSelector((state) => { return state.user.myInfo })
+
     const togetherList = async () => {
-        const res = api.get(`/product/hailey_ha`)
+        const res = api.get(`/product/${myInfo.accountname}/?limit=Number&skip=Number`)
         console.log(res);
     }
 
@@ -16,7 +19,7 @@ export default function Together() {
             <TogetherSection>
                 <TogetherWrap>
                     <TogetherList></TogetherList>
-                    {/* <div onClick={togetherList}>together</div> */}
+                    <div onClick={togetherList}>together</div>
                 </TogetherWrap>
             </TogetherSection>
         </>
