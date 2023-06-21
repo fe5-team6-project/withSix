d8a9cdc6f965967116bc92142e1e3cd5ffe15a4e
Gyuuul <kknd2848@naver.com>
2023-06-21 17:21:22 +0900

    #60 Feat: 이미지 등록 수정

--- src/pages/postUpload/postUpload.jsx
+++ src/pages/postUpload/postUpload.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-// import { Helmet } from "react-helmet-async";
 import Common from "../../components/main/Common" 
 import {
     PostHeaderSec,
     TitleSec,
-    // BackBtn,
     Tit,
     SubTit,
     UploadSec,
@@ -20,8 +18,10 @@ import {
     FileInput,
     FileTxt,
     FileSubTxt,
+    UploadSubSec,
 } from "./postStyle";
 import {PostUploadFooter} from './UploadFooter/uploadfooter'
+
 export default function PostUpload (){
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZkN2I3YjJjYjIwNTY2MzJkMDA5MyIsImV4cCI6MTY5MDgxNzQyOCwiaWF0IjoxNjg1NjMzNDI4fQ.fuRi1qVjgU4C7my-RPJrPOoBFjAvSHauogh8alP9mbI';
     const [content, setContent] = useState(""); // 게시글 입력 내용
@@ -30,7 +30,6 @@ export default function PostUpload (){
     const [uploadBtn, setUploadBtn] = useState(true);
     const fileInput = useRef(null);
     const navigate = useNavigate();
-    // const [imageSrc, setImageSrc] = useState('');
 
         const data = {
         post: {
@@ -44,22 +43,25 @@ export default function PostUpload (){
         const url = "https://api.mandarin.weniv.co.kr/";
         const formData = new FormData();
         formData.append("image", file);
+        console.log(file);
 
         const response = await axios.post(
             "https://api.mandarin.weniv.co.kr/image/uploadfile",
             formData
         );
         const imgName = url + response.data.filename;
-        const imgNameList = [];
-        imgNameList.push(imgName);
-        console.log(imgNameList);
+        // const imgNameList = [];
+        // imgNameList.push(imgName);
+        // console.log(imgNameList);
 
-        return imgNameList;
+        return imgName;
     }
 
     // 2. 이미지 미리보기 부분
     function ImgView(e) {
+        // 내가 이미지 등록에서 선택한 그 사진!
         const files = e.target.files;
+        // console.log(files);
         let fileUrls = [...showImg];
         let fileImgs = [...postImg];
 
@@ -122,7 +124,7 @@ export default function PostUpload (){
         data.post.content = content;
 
         try {
-            await axios
+            const res = await axios
                 .post("https://api.mandarin.weniv.co.kr/post", data, {
                     headers: {
                         Authorization: `Bearer ${token}`,
@@ -131,6 +133,7 @@ export default function PostUpload (){
                 })
                 // 전 페이지로
                 .then(navigate('/home'));
+                console.log(res);
         } catch (error) {
             console.log(error);
         }
@@ -147,10 +150,8 @@ export default function PostUpload (){
 
     const page = (        
     <>
-        {/* <Helmet> */}
             <title> 게시물 업로드 </title>
             <meta name="description" content="게시물 업로드 페이지" />
-        {/* </Helmet> */}
     
         {/* 게시물 업로드 헤더 section */}
         <UploadSec>
@@ -161,10 +162,6 @@ export default function PostUpload (){
                 <SubTit>글과 사진을 남기고 공유할 수 있습니다.</SubTit>
                 </TitleSec>
             </PostHeaderSec>   
-
-        {/* 프로필 불러오기 */}
-        {/* 프로필 이미지가 있을시에 setImageSrc 불러옴 */}
-            {/* {imageSrc && <img src={imageSrc} alt="" />} */}
 
         {/* 게시글 부분 */}
             <Lable htmlFor="post"/> 
@@ -179,47 +176,50 @@ export default function PostUpload (){
                 }}
             />
 
-        {/* 이미지 부분 */}
-            <PostUploadImg>
-                {showImg.length === 1
-                    ? showImg.map((image, id) => {
-                            return (
-                                <div key={id}>
-                                    <SingleImg key={id} src={image} />
-                                    <DeleteBtn
-                                        onClick={() => {
-                                            return DeleteImg(id);
-                                        }}
-                                    />
-                                </div>
-                            );
-                        })
-                    : showImg.map((image, id) => {
-                            return (
-                                <div key={id}>
-                                    <Img key={id} src={image} />
-                                    <DeleteBtn
-                                        onClick={() => {
-                                            return DeleteImg(id);
-                                        }}
-                                    />
-                                </div>
-                            );
-                        })}
-            </PostUploadImg>
+            <UploadSubSec>
+                {/* 이미지 등록 라벨 / 인풋 */}
+                    <FileUpload htmlFor="input-file">
+                        {/* <FileTxt>이미지 등록 <br/></FileTxt>
+                        <FileSubTxt>( n*n, 1M 이하, jpeg, png )</FileSubTxt> */}
+                        <FileInput
+                            id="input-file"
+                            name="PostImg"
+                            type="file"
+                            accept=".png, .jpg, .jpeg"
+                            onChange={ImgView}
+                            ref={fileInput}
+                        />
+                    </FileUpload>
 
-            <FileUpload htmlFor="input-file">
-                <FileTxt>이미지 등록 <br/></FileTxt>
-                <FileSubTxt>( n*n, 1M 이하, jpeg, png )</FileSubTxt>
-                <FileInput
-                    id="input-file"
-                    name="PostImg"
-                    type="file"
-                    accept=".png, .jpg, .jpeg"
-                    onChange={ImgView}
-                    ref={fileInput}
-                />
-            </FileUpload>
+                {/* 미리보기 이미지 부분 */}
+                    <PostUploadImg>
+                        {showImg.length === 1
+                            ? showImg.map((image, id) => {
+                                    return (
+                                        <div key={id}>
+                                            <SingleImg key={id} src={image} />
+                                            <DeleteBtn
+                                                onClick={() => {
+                                                        return DeleteImg(id);
+                                                }}
+                                            />
+                                        </div>
+                                    );
+                                })
+                            : showImg.map((image, id) => {
+                                    return (
+                                        <div key={id}>
+                                            <Img key={id} src={image} />
+                                            <DeleteBtn
+                                                onClick={() => {
+                                                    return DeleteImg(id);
+                                                }}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                    </PostUploadImg>
+            </UploadSubSec>
 
         {/* 업로드 버튼 부분 */}
             <PostUploadFooter UploadPost={UploadPost} disabled={uploadBtn ? null : 'disabled'} />
