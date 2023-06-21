9af05e6e24e01977517a14f3eb005c776188ae00
hailey ham <roserealslow@gmail.com>
2023-06-21 11:29:31 +0900

    #54 Feat: 모임상세페이지 마크업

--- src/pages/together/TogetherDetail.jsx
+++ src/pages/together/TogetherDetail.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import { React, useState } from 'react';
+import Common from '../../components/main/Common';
+import { styled } from 'styled-components';
+import initialImage from '../../assets/images/initialImage.png'
+import { useSelector, useDispatch } from 'react-redux';
+import { inputTogether } from '../../store/slices/togetherSlice';
+import { api, urlApi } from '../../lib/apis/axiosConfig';
 
 export default function TogetherDetail() {
+
+    const [togetherDetail, setTogetherDetail] = useState([]);
+
+    const togetherDetails = async () => {
+        const res = await api.get(`/product/detail/64912abfb2cb20566341bccf`);
+        console.log(res);
+        const abc = res.data.product;
+        const c = [...abc];
+        console.log(c);
+        // setTogetherDetail([abc]);
+    }
+    const page = (
+        <>
+            <Form>
+                <GroupHeader>
+                    <H1 onClick={togetherDetails}>모임 등록</H1>
+                </GroupHeader>
+                <GroupWrapper>
+                    <GroupImg></GroupImg>
+                    <GroupText>힝힝/</GroupText>
+                    <GroupText>50,000원</GroupText>
+                    <GroupDetailInfo>모임소개글</GroupDetailInfo>
+                </GroupWrapper>
+                <GroupBtn>버튼</GroupBtn>
+                {/* <RegiButton onClick={() => { }}>등록</RegiButton> */}
+            </Form>
+        </>
+    );
+
     return (
-        <div>TogetherDetail</div>
-    )
+        <>
+            <Common page={page}></Common>
+        </>
+    );
 }
+
+const Form = styled.section`
+    padding: 20px;
+    text-align: center;
+`;
+
+const GroupHeader = styled.header`
+    margin: 20px;
+`;
+
+const H1 = styled.h1`
+    font-size: var(--fsize-title);
+    color: var(--color-black);
+`;
+
+const GroupText = styled.p`
+    font-size: var(--fsize-m);
+    color: var(--color-black);
+    font-weight: bold;
+    text-align: left;
+    margin-top:34px;
+    text-indent: 10px;
+    &:after{
+        content: '';
+        display:block;
+        border-bottom:1px solid var(--color-main);
+        margin-top:8px;
+    }
+`;
+
+const GroupDetailInfo = styled.textarea`
+    width: 100%; //조정예정
+    height: 100px;
+    margin-top: 20px;
+    padding: 15px 10px;
+    font-size: var(--fsize-s);
+    font-weight: lighter;
+    box-sizing: border-box;
+    border: solid 1px var(--color-main);
+    border-radius: var(--radius-s);
+    resize: none; //크기조절 삭제
+    font-family: inherit;
+    background-color: inherit;
+    margin-bottom:50px;
+`
+
+const GroupWrapper = styled.div`
+    /* box-shadow: 0 0 3px 0 black; */
+`;
+
+const GroupImg = styled.img`
+    width: 100%;
+    height: 200px;
+    background-color: #ddd;
+    object-fit: cover;
+    border: none;
+    border-radius: var(--radius-s);
+`
+
+const GroupBtn = styled.button`
+    
+`
