964a0e1e3a15ef70189249c797044e1e08b643e8
hailey ham <roserealslow@gmail.com>
2023-06-22 15:57:28 +0900

    #50 Refactor : 팔로잉 리스트 li  컴포넌트 분리

--- src/pages/follow/FollowingList.jsx
+++ src/pages/follow/FollowingList.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import Common from '../../components/main/Common';
 import { styled } from "styled-components";
-import FollowButton from '../../components/follow/FollowButton';
+import FollowList from '../../components/follow/FollowList';
 
 export default function Following() {
     const page = (
         <>
             <Main>
                 <FollowWrap>
-                    <FollowList>
-                        <ProfileImg></ProfileImg>
-                        <TextWrap>
-                            <ProfileTitle>햄밍이당</ProfileTitle>
-                            <ProfileContent>난 세상에서 제일 빛나는 사람이 될 거야!</ProfileContent>
-                        </TextWrap>
-                        <FollowButton id="FollowBtn">팔로우</FollowButton>
-                    </FollowList>
+                    <FollowList></FollowList>
                 </FollowWrap>
             </Main>
         </>
@@ -36,40 +29,4 @@ const FollowWrap = styled.ul`
     display:flex;
     flex-direction: column;
     gap:20px;
-`
-
-const FollowList = styled.li`
-    display:flex;
-    width:100%;
-    height:50px;
-    background: #fff;
-    cursor: pointer;
-    align-items: center;
-    gap:14px;
-`
-
-const ProfileImg = styled.img`
-    width:50px;
-    height:50px;
-    border-radius: 50%;
-    object-fit: cover;
-    background: red;
-`
-
-const TextWrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex:1;
-    flex-basis : 290px;
-    flex-shrink: 0;
-`
-
-const ProfileTitle = styled.strong`
-    color: var(--color-black);
-    font-weight: bold;
-`
-
-const ProfileContent = styled.p`
-    font-size : var(--fsize-s);
-    color: var(--color-gray);
 `
