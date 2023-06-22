beaa90dcb03c72c5d4129615e0dbc946df31ebfa
hailey ham <roserealslow@gmail.com>
2023-06-22 10:07:57 +0900

    #53 Feat : 모임 수정페이지 수정 버튼 연결

--- src/components/together/TogetherEditButton.js
+++ src/components/together/TogetherEditButton.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 import { styled } from 'styled-components'
 
 
 export default function TogetherEditButton() {
+    const id = useParams().id
+    const navigate = useNavigate();
     return (
         <>
-            <EditButton id="editBtn">수정</EditButton>
+            <EditButton id="editBtn" onClick={() => { navigate(`/together/edit/${id}`) }}>수정</EditButton>
         </>
     )
 }
