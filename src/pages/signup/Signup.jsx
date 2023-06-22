1c63ab23c5d2e7ad162855933f6c407984964879
ungHeung <t01047377477@gmail.com>
2023-06-22 22:34:38 +0900

    #73 모달 상태관리 변경에 따른 코드 리팩 회원가입

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,26 +2,66 @@ import { useEffect, useState } from 'react';
 import EmailPassword from './EmailPassword';
 import Profile from './Profile';
 import Common from '../../components/main/Common';
+import { useDispatch } from 'react-redux';
+import {
+    setContent,
+    setIsVisible,
+    setUrl,
+} from '../../store/slices/modalSlice';
 
 export default function Signup() {
+    const dispatch = useDispatch();
     const [passStep, setPassStep] = useState(false);
     const [user, setUser] = useState({
         username: String,
         email: String,
     });
 
+    const setModalContent = (props) => {
+        dispatch(
+            setContent({
+                state: props.state,
+                message: props.message,
+            })
+        );
+    };
+    const setModalUrl = (url) => {
+        dispatch(setUrl({ url: url }));
+    };
+    const setModalVisible = (isVisible) => {
+        dispatch(setIsVisible({ isVisible: isVisible }));
+    };
+
     const [page, setPage] = useState(
-        <EmailPassword passValid={setPassStep} userData={setUser} />
+        <EmailPassword
+            passValid={setPassStep}
+            userData={setUser}
+            setModalContent={setModalContent}
+            setModalUrl={setModalUrl}
+            setModalVisible={setModalVisible}
+        />
     );
 
     useEffect(() => {
         if (!passStep) {
             setPage(
-                <EmailPassword passStep={setPassStep} userData={setUser} />
+                <EmailPassword
+                    passStep={setPassStep}
+                    userData={setUser}
+                    setModalContent={setModalContent}
+                    setModalUrl={setModalUrl}
+                    setModalVisible={setModalVisible}
+                />
             );
         } else {
-            console.log(user);
-            setPage(<Profile userData={user} />);
+            setPage(
+                <Profile
+                    userData={user}
+                    setModalContent={setModalContent}
+                    setModalUrl={setModalUrl}
+                    setModalVisible={setModalVisible}
+                />
+            );
         }
     }, [passStep]);
 
