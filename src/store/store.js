e20ea8a2f3f98e1faaaf19e9e720f29801a8cc6f
ungHeung <t01047377477@gmail.com>
2023-06-22 17:09:37 +0900

    Feat : 모달 리덕스 상태관리 슬라이스 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { user } from './slices/userSlice';
 import { together } from './slices/togetherSlice';
+import { modal } from './slices/modalSlice';
 
 export default configureStore({
     reducer: {
         user: user.reducer,
-        together: together.reducer
+        together: together.reducer,
+        modal: modal.reducer,
     },
 });
