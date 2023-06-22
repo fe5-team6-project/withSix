406d4de7e1ec21ca7581969559b1710c577201c2
hailey ham <roserealslow@gmail.com>
2023-06-22 16:40:27 +0900

    #50 팔로잉 리스트 API 연결 & :accountname params

--- src/pages/follow/FollowingList.jsx
+++ src/pages/follow/FollowingList.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Common from '../../components/main/Common';
 import { styled } from "styled-components";
 import FollowList from '../../components/follow/FollowList';
+import { api } from '../../lib/apis/axiosConfig';
+import { useParams } from 'react-router-dom';
 
 export default function Following() {
+    const accountName = useParams().accountname;
+    const FollowingList = async () => {
+        const res = api.get(`profile/${accountName}/following`);
+        console.log(res);
+    }
     const page = (
         <>
             <Main>
-                <FollowWrap>
+                <FollowWrap><p onClick={FollowingList}>click</p>
                     <FollowList></FollowList>
                 </FollowWrap>
             </Main>
