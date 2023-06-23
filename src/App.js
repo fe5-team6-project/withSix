d5dfadb3b55e0afc679a83485a704f90b393952b
ungHeung <t01047377477@gmail.com>
2023-06-23 10:45:52 +0900

    Feat : 라이브러리 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Router from "./route/route";
-import './reset.css'
-import './common.css'
+import Router from './route/route';
+import './reset.css';
+import './common.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
     return (
