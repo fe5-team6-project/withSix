78e92cb79480de1a69b079d6ffeffe0611826b4d
hailey ham <roserealslow@gmail.com>
2023-06-24 16:07:47 +0900

    #50 Refactor : 팔로우목록 해당프로필 넘어가기 코드수정

--- src/components/follow/FollowList.js
+++ src/components/follow/FollowList.js
@@ -2,17 +2,29 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import FollowButton from '../../components/follow/FollowButton';
 import { styled } from 'styled-components';
+import getUserProfile from '../../pages/userprofile/getUserProfile';
+import { setUserInfo } from '../../store/slices/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function FollowList({ accountname, username, intro, image, isfollow }) {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    async function setUser() {
+        const user = await getUserProfile(accountname);
+        dispatch(setUserInfo(user));
+    }
+
     return (
         <>
             <FollowItem>
-                <ProfileImg src={image} onClick={() => { navigate(`/profile/${accountname}`) }}></ProfileImg>
-                <TextWrap onClick={() => { navigate(`/profile/${accountname}`) }}>
-                    <ProfileTitle>{username}</ProfileTitle>
-                    <ProfileContent>@ {accountname}</ProfileContent>
-                </TextWrap>
+                <ProfileWrap onClick={async (e) => { e.stopPropagation(); await setUser(); navigate(`../profile/${accountname}`); }} >
+                    <ProfileImg src={image} />
+                    <TextWrap>
+                        <ProfileTitle>{username}</ProfileTitle>
+                        <ProfileContent>@ {accountname}</ProfileContent>
+                    </TextWrap>
+                </ProfileWrap>
                 <FollowButton id="FollowBtn" accountname={accountname} isfollow={isfollow}></FollowButton>
             </FollowItem>
         </>
@@ -20,20 +32,25 @@ export default function FollowList({ accountname, username, intro, image, isfoll
 }
 
 const FollowItem = styled.li`
-    display:flex;
-    width:100%;
-    height:50px;
     background: #fff;
-    cursor: pointer;
+    display:flex;
     align-items: center;
-    gap:14px;
     &>button{
         width:58px;
         height: 28px;
         font-size: var(--fsize-s);
         font-weight: lighter;
     }
-`
+    `
+const ProfileWrap = styled.div`
+    width:100%;
+    height:50px;
+    display:flex;
+    align-items: center;
+    cursor: pointer;
+    gap:14px;
+    justify-content: space-between;
+    `
 
 const ProfileImg = styled.img`
     width:50px;
