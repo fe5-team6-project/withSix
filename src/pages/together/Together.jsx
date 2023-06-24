c1cafe562d13f779eabe13ead8ab3dd6f130f5ba
hailey ham <roserealslow@gmail.com>
2023-06-25 04:37:13 +0900

    Comment : 불필요한 주석제거 및 코드 삭제

--- src/pages/together/Together.jsx
+++ src/pages/together/Together.jsx
@@ -3,46 +3,34 @@ import { api } from '../../lib/apis/axiosConfig'
 import { styled } from 'styled-components';
 import Common from '../../components/main/Common';
 import TogetherList from '../../components/together/TogetherList';
-import { useSelector } from 'react-redux';
-import WriteButton from '../../components/writebutton/WriteButton'
+import WriteButton from '../../components/writebutton/WriteButton';
 import { useParams } from 'react-router';
 
 
 export default function Together() {
     const accountname = useParams().id;
     const [pages, setPages] = useState(10);
-    //추후 '더보기'추가예정
-
     const [togetherList, setTogetherList] = useState([]);
 
     useEffect(() => {
         async function axiosTogetherList() {
             const res = await api.get(`/product/${accountname}/?limit=${pages}&skip=0`);
-            const abc = res?.data?.product;
-            setTogetherList([...abc]);
-            console.log(abc)
+            const data = res?.data?.product;
+            setTogetherList([...data]);
+            console.log(data)
         }
         axiosTogetherList();
     }, [pages]);
-
 
     const page = (
         <>
             <TogetherSection>
                 <TogetherWrap>
                     {!togetherList ? [] : togetherList.map((item) => (
-                        //item = {item}해도 되는데 어떻게 할 지 아직은 고민
                         <TogetherList key={item.id} {...item}></TogetherList>
                     ))}
-                    {/* {togetherList.map((item) => (
-                        <TogetherList></TogetherList>
-                    ))} */}
                 </TogetherWrap>
-                <MoreButton
-                    onClick={() => setPages((pagse) => pages + 5)}
-                >
-                    더보기
-                </MoreButton>
+                <MoreButton onClick={() => setPages((pagse) => pages + 5)}>더보기</MoreButton>
                 <WriteButton url={`/together/upload`} />
             </TogetherSection>
         </>
