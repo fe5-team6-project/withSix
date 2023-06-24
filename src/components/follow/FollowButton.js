5b0c729cd8ef60244e202be8913d329c1b12df88
hailey ham <roserealslow@gmail.com>
2023-06-24 16:28:34 +0900

    #24 Refactor : 코드 중복 제거

--- src/components/follow/FollowButton.js
+++ src/components/follow/FollowButton.js
@@ -1,46 +1,23 @@
-// import React, { useState, useEffect } from 'react'
-import { styled } from 'styled-components'
-// import HandleFollow from './HandleFollow'
-import { api } from '../../lib/apis/axiosConfig'
-import { useState } from 'react';
+import { styled } from 'styled-components';
+import { api } from '../../lib/apis/axiosConfig';
 
 export default function FollowButton({ accountname, isfollow }) {
-    const [isFollow, setIsFollow] = useState(isfollow);
-    //팔로우 상태이면 : 버튼에 '언팔로우'뜨기 +(버튼누르기)> 언팔로우 처리 + '팔로우'로 버튼 변경
     const handleFollow = async () => {
-        if (isFollow) {
-            await api.delete(`/profile/${accountname}/unfollow`, [])
-                .then((res) => {
-                    console.log(res);
-                    setIsFollow(res.data.profile.isfollow);
-                })
-                .then(() => {
-                    console.log(isFollow);
-                });
+        if (isfollow) {
+            await api
+                .delete(`/profile/${accountname}/unfollow`, [])
+                .then((res) => { console.log(res); })
         } else {
             await api.post(`profile/${accountname}/follow`, [])
-                .then((res) => {
-                    console.log(res);
-                    setIsFollow(res.data.profile.isfollow);
-                })
-                .then(() => {
-                    console.log(isFollow);
-                })
+                .then((res) => { console.log(res); })
         }
-    }
-
+    };
 
     return (
-        // <StyledButton onClick={HandleFollow}>Follow</StyledButton>
-        // <FollowUnButton id="FollowBtn" onClick={follow}>팔로우</FollowUnButton>
-        // <FollowUnButton id="FollowBtn" onClick={handleFollow}>
-        //     {isFollow ? '언팔로우' : '팔로우'}
-        // </FollowUnButton>
         <FollowUnButton onClick={handleFollow}>
-            {isFollow ? '언팔로우' : '팔로우'}
+            {isfollow ? '언팔로우' : '팔로우'}
         </FollowUnButton>
-    )
+    );
 }
 
-const FollowUnButton = styled.button`
-`
+const FollowUnButton = styled.button``;
