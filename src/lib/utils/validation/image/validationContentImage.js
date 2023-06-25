2902f05efceae106aeecbf51e1315a0f4ef0eab7
ungHeung <t01047377477@gmail.com>
2023-06-26 01:35:39 +0900

    Feat : 이미지 유효성 검사 기능

--- src/lib/utils/validation/image/validationContentImage.js
+++ src/lib/utils/validation/image/validationContentImage.js
@@ -1,6 +1,30 @@
-import togetherImg from '../../../../assets/images/togetherImg.png'
+import togetherImg from '../../../../assets/images/togetherImg.png';
 
-export function validationContentImage(image) { }
+export function validationContentImage(image) {
+    let src = image;
+
+    if (!src) {
+        src = undefined;
+        return src;
+    }
+
+    if (!src.includes('api')) {
+        src = `${URL}/${src}`;
+        return src;
+    }
+
+    if (src.includes('api')) {
+        src = undefined;
+        return;
+    }
+
+    if (src.split('://').length > 2) {
+        src = undefined;
+        return;
+    }
+
+    return src;
+}
 
 export function emptyContentImage(e) {
     e.currentTarget.style.display = 'none';
