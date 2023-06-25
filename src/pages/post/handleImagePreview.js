d7a7c6ef23fe05d45de1bb0e11ae373230720f68
ungHeung <t01047377477@gmail.com>
2023-06-25 22:11:33 +0900

    Feat : 이미지 미리보기 값, 이미지 URL값 따로 반환하는 기능

--- src/pages/post/handleImagePreview.js
+++ src/pages/post/handleImagePreview.js
@@ -1,6 +1,11 @@
+import { BASE_URL } from '../../lib/apis/constants';
+
 export default function handleImagePreview(e) {
     const images = e.target.files;
+    const imageUrl = `${BASE_URL}/${images[0].lastModified}`;
+    console.log(images);
+
     if (!images.length) return;
 
-    return [URL.createObjectURL(images[0]), images[0]];
+    return [URL.createObjectURL(images[0]), imageUrl];
 }
