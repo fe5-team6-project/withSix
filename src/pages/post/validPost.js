83e492aecea0d53eccf1cc0b8d8c0fd7c7721141
ungHeung <t01047377477@gmail.com>
2023-06-26 10:49:51 +0900

    Feat : 모달 출력 기능 추가

--- src/pages/post/validPost.js
+++ src/pages/post/validPost.js
@@ -1,17 +1,25 @@
-import { FAIL_NULL_CONTENT } from '../../lib/apis/constant/message';
+import {
+    FAIL_NULL_CONTENT,
+    POST_UPLOAD_OK,
+} from '../../lib/apis/constant/message';
 
 const result = {
     state: false,
     message: String,
 };
 
-export const validPostContent = (content) => {
-    const length = content.length;
+export const validPostContent = (contents) => {
+    const length = contents?.length;
 
     if (!length) {
         result.state = false;
         result.message = FAIL_NULL_CONTENT;
+        return result;
     }
+
+    result.state = true;
+    result.message = POST_UPLOAD_OK;
+    return result;
 };
 
 export const validPostImage = (image) => {};
