5a0939ffcc9db1e96303483c4dbbdf451fd6f018
ungHeung <t01047377477@gmail.com>
2023-06-28 00:17:52 +0900

    Feat : 투게더 목록 스플래시 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,7 +19,6 @@ export default function Home() {
 
     const [isMyPost, setIsMyPost] = useState(id ? false : true);
     const [isSplash, setIsSplash] = useState(true);
-    console.log(isMyPost);
 
     useEffect(() => {
         async function fetchData() {
