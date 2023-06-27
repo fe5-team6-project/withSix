d2d78cd79dd9c7650510be2e6ef7ee044667dd48
ungHeung <t01047377477@gmail.com>
2023-06-28 00:06:50 +0900

    Feat : 기본 이미지 변경

--- src/lib/apis/constant/path.js
+++ src/lib/apis/constant/path.js
@@ -1,3 +1,6 @@
 /** path */
 export const URL = 'https://api.mandarin.weniv.co.kr';
-export const DEFAULT_IMAGE = 'http://146.56.183.55:5050/Ellipse.png';
+export const DEFAULT_IMAGE =
+    'https://api.mandarin.weniv.co.kr/1687875212093.png';
+export const DEFAULT_IMAGE_BOARD =
+    'https://api.mandarin.weniv.co.kr/1687876480654.png';
