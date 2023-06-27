b531e8d2ae16e72e9c5d8bae3553a9c57960609a
ungHeung <t01047377477@gmail.com>
2023-06-28 02:27:06 +0900

    Design : 리스트 이미지 배경색 제거

--- src/components/together/TogetherList.js
+++ src/components/together/TogetherList.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-import { handleErrorImg } from '../../lib/utils/validation/image/validationContentImage'
+import { handleErrorImg } from '../../lib/utils/validation/image/validationContentImage';
 
 export default function TogetherList({ id, itemName, price, link, itemImage }) {
     const navigate = useNavigate();
 
     return (
         <>
-            <TogetherItem onClick={() => {
-                navigate(`/together/detail/${id}`);
-            }}>
+            <TogetherItem
+                onClick={() => {
+                    navigate(`/together/detail/${id}`);
+                }}
+            >
                 <TogetherImage src={itemImage} onError={handleErrorImg} />
                 <TogetherName>{itemName}</TogetherName>
-                <TogetherPrice><span>{price.toLocaleString('ko-KR')}</span>원</TogetherPrice>
+                <TogetherPrice>
+                    <span>{price.toLocaleString('ko-KR')}</span>원
+                </TogetherPrice>
             </TogetherItem>
         </>
     );
@@ -22,21 +26,21 @@ export default function TogetherList({ id, itemName, price, link, itemImage }) {
 const TogetherItem = styled.li`
     display: flex;
     justify-content: space-between;
-    flex-wrap:wrap;
-    flex:1;
-    min-width:160px;
-    height:130px;
+    flex-wrap: wrap;
+    flex: 1;
+    min-width: 160px;
+    height: 130px;
     background: #fff;
     align-items: center;
     gap: 10px;
     border-radius: 5px;
-    padding:10px;
+    padding: 10px;
     cursor: pointer;
-    &:nth-of-type(1){
+    &:nth-of-type(1) {
         flex-basis: 100%;
-        height:240px;
+        height: 240px;
         & img {
-            height:200px;
+            height: 200px;
         }
         & p {
             font-size: var(--fsize-s);
@@ -44,48 +48,47 @@ const TogetherItem = styled.li`
             & > span {
                 overflow: hidden;
                 text-overflow: ellipsis;
-                max-width:90px;
-                display:inline-block;
+                max-width: 90px;
+                display: inline-block;
             }
         }
         & strong {
             overflow: hidden;
             text-overflow: ellipsis;
-            max-width:210px;
+            max-width: 210px;
         }
     }
-`
+`;
 
 const TogetherImage = styled.img`
-    width:100%;
+    width: 100%;
     height: 100px;
-    background: yellow;
     border-radius: 5px;
     object-fit: cover;
-`
+`;
 const TogetherName = styled.strong`
     font-size: var(--fsize-m);
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    max-width:90px;
+    max-width: 90px;
 `;
 
 const TogetherPrice = styled.p`
     font-size: var(--fsize-s);
     color: var(--color-gray);
-    display:inline-block;
-    line-height:13px;
+    display: inline-block;
+    line-height: 13px;
 
     & > span {
-        display:inline-block;
+        display: inline-block;
         color: var(--color-main);
         overflow: hidden;
         text-overflow: ellipsis;
-        max-width:45px;
+        max-width: 45px;
         white-space: nowrap;
         line-height: 13px;
         vertical-align: bottom;
-        margin-right:2px;
+        margin-right: 2px;
     }
 `;
