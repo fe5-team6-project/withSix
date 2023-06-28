573cc072035d7bd9d09a6bcf3c2708953c1f5423
ungHeung <t01047377477@gmail.com>
2023-06-28 18:30:08 +0900

    Fix : 배경 삭제

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -3,7 +3,7 @@ import Common from '../../components/main/Common';
 import { styled } from 'styled-components';
 import { useSelector } from 'react-redux';
 import { DEFAULT_IMAGE } from '../../lib/apis/constant/path';
-import chatBack from '../../assets/images/common/splash-login.gif';
+// import logo from '../../assets/logo/LOGO-full-negative.svg';
 
 export default function Chat() {
     const user = useSelector((state) => state.user?.myInfo);
@@ -93,17 +93,19 @@ export default function Chat() {
 const ChatWrap = styled.section`
     width: 100%;
     padding-top: 120px;
-    background: url(${chatBack}) no-repeat;
-    background-size: 100%;
-    background-position: 50% 120%;
+    /* background: url() no-repeat;
+    background-size: 70%;
+    background-position: 50% 60%; */
 `;
 
 const ChatList = styled.ul`
     width: 350px;
-    max-height: 580px;
+    height: 580px;
     margin: 0px auto;
     padding: 0 20px;
     overflow-y: auto;
+    /* border-top: 2px solid var(--color-disabled); */
+    /* border-bottom: 2px solid var(--color-disabled); */
 `;
 
 const ChatItem = styled.li`
