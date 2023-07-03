263fb64c5b064601890ae9243420f651b2a6b7ce
ungHeung <t01047377477@gmail.com>
2023-07-04 03:00:01 +0900

    Refactor : 유저 정보 가져오는 통신 fetch에서 axios로 변경

--- src/pages/login/getMyInfo.js
+++ src/pages/login/getMyInfo.js
@@ -1,22 +1,42 @@
+import axios from 'axios';
 import { URL } from '../../lib/apis/constant/path';
 
 export default async function getMyInfo() {
     const requestPath = '/user/myinfo';
     const requestUrl = `${URL}${requestPath}`;
 
-    const token = await localStorage.token;
+    const token = localStorage.token;
     const bearerToken = `Bearer ${token}`;
 
-    const response = await fetch(requestUrl, {
+    // const response = await fetch(requestUrl, {
+    //     method: 'GET',
+    //     headers: {
+    //         Authorization: bearerToken,
+    //         'Content-type': 'application/json',
+    //     },
+    //     body: JSON.stringify(),
+    // });
+
+    // const json = await response.json();
+
+    // return json.user;
+
+    const response = await axios({
         method: 'GET',
+        url: requestUrl,
         headers: {
             Authorization: bearerToken,
             'Content-type': 'application/json',
         },
-        body: JSON.stringify(),
     });
 
-    const json = await response.json();
+    const data = await response.data;
 
-    return json.user;
+    axios.interceptors.request.use((config) => {
+        const token = localStorage.token;
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    });
+
+    return data.user;
 }
