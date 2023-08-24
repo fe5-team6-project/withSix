6083a9ac886f046264f05074dd158684e37a8fa8
ungHeung <t01047377477@gmail.com>
2023-08-24 18:56:49 +0900

    rename : js -> ts 파일로 변경

---
+++ src/lib/apis/constant/message.ts
@@ -0,0 +1,42 @@
+/** modal message */
+export const FAIL_ACCESS = '잘못된 접근입니다.';
+
+/** 회원 정보, 로그인 관련 */
+export const LOGIN_OK = '로그인 성공!';
+export const LOGOUT_OK = '로그아웃 성공!';
+export const SIGNUP_OK = '가입 성공!';
+export const PROFILE_UPDATE_OK = '프로필 수정 성공!';
+
+export const FAIL_ALEADY_EMAIL = '이미 등록된 이메일입니다.';
+export const FAIL_ALEADY_ID = '이미 사용중인 아이디입니다.';
+export const FAIL_FORM_EMAIL = '올바른 이메일 형식이 아닙니다.';
+export const FAIL_FORM_PASSWORD = '하나 이상의 문자, 숫자가 포함되어야 합니다.';
+export const FAIL_FORM_ID = '영문, 숫자, (_), (,)만 사용할 수 있습니다.';
+export const FAIL_NULL_EMAIL = '이메일을 입력해주세요!';
+export const FAIL_NULL_PASSWORD = '비밀번호를 입력해주세요!';
+export const FAIL_NULL_ID = '아이디를 입력해주세요!';
+export const FAIL_NULL_NAME = '닉네임을 입력해주세요!';
+export const FAIL_LENGTH_PASSWORD = '비밀번호는 6자 이상 16자 이하여야 합니다.';
+export const FAIL_CHECK_PASSWORD = '비밀번호를 확인해주세요.';
+export const FAIL_VALID_EMAIL_PASSWORD = '이메일 또는 비밀번호가 다릅니다.';
+export const FAIL_LOAD_PROFILE = '프로필 정보를 불러오지 못했습니다.';
+
+/** 게시물 관련 */
+export const POST_UPLOAD_OK = '포스트 업로드 성공!';
+export const TOGETHER_UPLOAD_OK = '모임 등록 성공!';
+export const CONTENT_UPDATE_OK = '내용 수정 성공!';
+export const CONTENT_DELETE_OK = '삭제 성공!';
+
+export const FAIL_NULL_CONTENT = '내용을 입력해 주세요!';
+export const FAIL_OVER_SIZE = '이미지 업로드 최대 용량은 10MB입니다.';
+export const FAIL_OVER_IMAGE = '사진은 최대 3장까지 등록이 가능합니다.';
+
+/** 모임 관련*/
+export const FAIL_NULL_PRICE = '가격을 입력해 주세요!';
+export const FAIL_INVALID_PRICE = '가격을 올바르게 입력해 주세요!';
+export const FAIL_NULL_TITLE = '모임명을 입력해 주세요!';
+export const FAIL_LENGTH_TITLE = '모임명은 2~15자 이내로 입력해 주세요!';
+
+/** 댓글 관련 */
+export const COMMENT_WRITE_OK = '댓글이 등록되었습니다.';
+export const COMMENT_DELETE_OK = '댓글이 삭제되었습니다.';
